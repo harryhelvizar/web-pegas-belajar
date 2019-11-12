656212d17d399579d3c1b349ce3aebd8f5137597
Lalu Abdurrahman <34390193+lalurahman@users.noreply.github.com>
2019-11-12 23:00:06 +0800

    update

--- application/views/front/footer.php
+++ application/views/front/footer.php
@@ -119,6 +119,7 @@
 <script src="<?= base_url('assets/') ?>plugins/parallax-js-master/parallax.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
+<script type='text/javascript' src='<?= base_url('assets/') ?>js/custom.js'></script>
 
 
 
