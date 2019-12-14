91782df2e8d8364680e278f87b24d9c4a5723f54
Muhdan Fyan Syah Sofian <muhdanfyan@gmail.com>
2019-12-14 10:33:27 +0800

    Update soal.php

--- application/views/soal.php
+++ application/views/soal.php
@@ -513,9 +513,9 @@ $this->load->view('front/header');
 
     <!-- loader -->
     <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px">
-            <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
-            <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00" /></svg></div>
-
+        <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
+        <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00" /></svg>
+    </div>
 
     <script src="<?= base_url('assets/template/') ?>js/jquery-migrate-3.0.1.min.js"></script>
     <script src="<?= base_url('assets/template/') ?>js/popper.min.js"></script>
@@ -530,8 +530,7 @@ $this->load->view('front/header');
     <script src="<?= base_url('assets/template/') ?>js/bootstrap-datepicker.js"></script>
     <script src="<?= base_url('assets/template/') ?>js/jquery.timepicker.min.js"></script>
     <script src="<?= base_url('assets/template/') ?>js/scrollax.min.js"></script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false">
-    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
     <script src="<?= base_url('assets/template/') ?>js/google-map.js"></script>
     <script src="<?= base_url('assets/template/') ?>js/main.js"></script>
 
