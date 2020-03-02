1f91b4df9c52ecdb5eecbc8634a66063690aa718
Lalu Abdurrahman <34390193+lalurahman@users.noreply.github.com>
2020-03-02 19:43:54 +0800

    update

--- application/views/profile-siswa.php
+++ application/views/profile-siswa.php
@@ -7,43 +7,19 @@
     <section class="ftco-section">
 
         <div class="container">
-            <div class="">
-                <div class="">
-                    <div class="container">
-                        <div class="row bg-light">
-                            <div class="col-md-4 mt-5">
-                                <h2>Haloo</h2>
-                            </div>
-                            <div class="col-md-4">
-                                <img src="<?= base_url('assets/template/') ?>images/pegasbelajar.jpg" height="255" width="250" class="mb-5" style="border-radius: 50%">
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div class="container">
+                <table class="table table-borderless ml-5">
+                    <tr>
+                        <td>Nama Lengkap</td>
+                        <td>:</td>
+                        <td>Lalu Abdurrahman</td>
+                    </tr>
+                </table>
             </div>
+
         </div>
     </section>
 
-    <section class="ftco-section">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-2"></div>
-                <div class="col-md-10">
-                    <div class="row">
-
-                        <div class="col-md-10 bg-light p-5 ftco-animate">
-                            <div class="text-center">
-                                <img src="<?= base_url('assets/template/') ?>images/pegasbelajar.jpg" height="255" width="250" class="mb-5" style="border-radius: 50%">
-                            </div>
-                            <p>Pegas Belajar merupakan media penelusuran gaya belajar siswa berbasis website, yang dikembangkan secara khusus untuk membantu siswa mengenali dan memahami preferensi gaya belajarnya. Pegas Belajar ini dibuat dengan memanfaatkan perkembangan IT, yang secara khusus diperuntukkan bagi siswa di sekolah menengah atas (SMA).</p>
-                            <p>Pegas Belajar ini adalah hasil dari penelitian pengembangan yang dilakukan oleh Ramli dengan judul penelitian “Pengembangan Pegas Belajar Sebagai Media Penelusuran Gaya Belajar Siswa di Sekolah Menengah Atas (SMA)” pada Program Pascasarjana Universitas Negeri Makassar.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
 
 
     <?php $this->load->view('front/footer') ?>
